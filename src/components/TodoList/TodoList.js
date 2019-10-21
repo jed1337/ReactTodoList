5f958421004b3dfe2f31f2639adf05e9679fad60
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-21 18:16:51 +0800

    Make inputText state work with TodoList

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,12 +1,24 @@
 import React from 'react'
 
 class TodoList extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state={
+            inputText: "",
+            todoItems:[]
+        }
+    }
+
+    updateInputText = (e) => {
+        this.setState({inputText: e.target.value})
+    };
+
     render() {
         return(
             <div>
-                <input type="text"/>
+                <input type="text" onChange={this.updateInputText}/>
                 <button>Add todo item</button>
-                <div>todo list goes here</div>
+                <div>{this.state.inputText}</div>
             </div>
         )
     }
