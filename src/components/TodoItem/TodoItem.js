9c48e3cf6a37c3fbffa5e2661ea6c142c7a390b4
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-22 09:27:01 +0800

    Turn TodoItem into a functional component

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './TodoItem.css';
 
-class TodoItem extends React.Component {
-    render() {
-        return (
-            <div>
-                <input type="checkbox"/>
-                <span className="strikethrough">{this.props.contents}</span>
-            </div>
-        )
-    }
+function TodoItem (props) {
+    return (
+        <div>
+            <input type="checkbox"/>
+            <span className="strikethrough">{props.contents}</span>
+        </div>
+    )
 }
-
 export default TodoItem;
