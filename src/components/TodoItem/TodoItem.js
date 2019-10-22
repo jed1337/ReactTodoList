95e1ae18d75fd18698f018e697754955e63eba84
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-22 11:18:09 +0800

    Add website design features

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './TodoItem.css';
 
-function TodoItem (props) {
-    return (
-        <div>
-            <input type="checkbox"/>
-            <span className="strikethrough">{props.contents}</span>
-        </div>
-    )
+class TodoItem extends React.Component{
+    randomClasses() {
+        const fontSizeClasses=["small", "medium", "large"];
+        const randomFont = fontSizeClasses[Math.floor(Math.random()*3)];
+
+        const fontColours=["pink", "yellow", "green"];
+        const randomColour = fontColours[Math.floor(Math.random()*3)];
+
+        return `strikethrough ${randomFont} ${randomColour}`;
+    }
+    render() {
+        return (
+            <div>
+                <input type="checkbox"/>
+                <span className={this.randomClasses()}>
+                    {this.props.contents}
+                </span>
+            </div>
+        );
+    }
 }
 export default TodoItem;
