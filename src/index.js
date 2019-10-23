1227603317501d8207eeab69ae68807070b82048
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-23 08:54:17 +0800

    Add reducers

--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import TodoList from "./components/TodoList/TodoList";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import rootReducer from "./components/reducers";
 
-ReactDOM.render(<TodoList/>, document.getElementById('root'));
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+ReactDOM.render(
+    <Provider store={store}>
+        <TodoList/>
+    </Provider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
