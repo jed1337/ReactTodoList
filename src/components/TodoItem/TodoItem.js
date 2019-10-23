7ff9de256645404608e3a5883ece03afb2388afd
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-23 08:49:40 +0800

    Revert styling js

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -2,20 +2,11 @@ import React from 'react';
 import './TodoItem.css';
 
 class TodoItem extends React.Component{
-    randomClasses() {
-        const fontSizeClasses=["small", "medium", "large"];
-        const randomFont = fontSizeClasses[Math.floor(Math.random()*3)];
-
-        const fontColours=["pink", "yellow", "green"];
-        const randomColour = fontColours[Math.floor(Math.random()*3)];
-
-        return `strikethrough ${randomFont} ${randomColour}`;
-    }
     render() {
         return (
             <div>
                 <input type="checkbox"/>
-                <span className={this.randomClasses()}>
+                <span className="strikethrough">
                     {this.props.contents}
                 </span>
             </div>
