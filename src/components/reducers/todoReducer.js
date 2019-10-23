fb6ad4fcff16e7b07453e903826c3d50b4ea008b
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-23 10:48:58 +0800

    Add comments for futher reference

--- src/components/reducers/todoReducer.js
+++ src/components/reducers/todoReducer.js
@@ -7,14 +7,21 @@ const initialState = {
 export default (state = initialState, action) => {
     switch (action.type) {
         case "ADD_TODO_ITEM":
-            // const newTodoItems = state.todoItems;
-            // newTodoItems.push({text: action.payload, id: generateId()});
+            //const newTodoItems = state.todoItems; newTodoItems refers to the state.todoItems.
+            //we shouldn't update the same item according to react
+            //
+            //const newTodoItems = [...state.todoItems]; creates a new array from state.todoItems
 
-            return {
-                ...state,
-                todoItems: [...state.todoItems, {text: action.payload, id: generateId()}]
-            };
-            // return{...state, todoItems: newTodoItems};
+            const newTodoItems = [...state.todoItems];
+            newTodoItems.push({text: action.payload, id: generateId()});
+
+            return{...state, todoItems: newTodoItems};
+
+            //shorter way
+                // return {
+                //     ...state,
+                //     todoItems: [...state.todoItems, {text: action.payload, id: generateId()}]
+                // };
 
         default:
             return state;
