de7897c19cd102bd06365e02c59582befcb869c1
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-23 16:55:40 +0800

    Remove other comments in Todo

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -2,21 +2,9 @@ import React from 'react';
 import './TodoItem.css';
 
 class TodoItem extends React.Component{
-    // constructor(props) {
-    //     super(props);
-    //     this.state={isCompleted: this.props.isCompleted === "completed"};
-    // }
-
     toggleChange = (event) => {
-        // this.setState({
-        //     isCompleted: !this.state.isCompleted
-        // })
-        // console.log("event target checked ", event.target.checked);
         const status = event.target.checked ? "completed" : "active";
-        // console.log("todo item id ", this.props.id);
-        // console.log("Status ", status);
         this.props.updateTodoItem(this.props.id, status);
-
     };
 
     render() {
